8e8721cd2d34e308e0e4d9d2e2d9b658b865cd2c
Matt Hamil <j.matthew.hamil@gmail.com>
2019-07-29 02:07:12 -0500

    feat: more prettier rules

--- packages/prettier/prettier.config.js
+++ packages/prettier/prettier.config.js
@@ -2,10 +2,18 @@
  * https://prettier.io/docs/en/configuration.html
  */
 module.exports = {
-  trailingComma: 'es5',
   tadWidth: 2,
+  useTabs: false,
   semi: false,
   singleQuote: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'es5',
+  jsxSingleQuote: false,
   printWidth: 100,
   arrowParens: 'always',
+  endOfLine: 'lf',
+  bracketSpacing: true,
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: 'preserve',
 }
