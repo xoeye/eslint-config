020d732c0ab09ac7fe7842bc82211d4aaa5beacb
Matt Hamil <j.matthew.hamil@gmail.com>
2019-07-28 23:52:35 -0500

    feat: react-native rules

--- packages/react-native/rules/a11y.js
+++ packages/react-native/rules/a11y.js
@@ -1,5 +1,39 @@
 /**
- * @todo
- *   Add a11y rules
+ * Accessibility rules for React Native
  */
-module.exports = {}
+module.exports = {
+  plugins: ['react-native-a11y'],
+  rules: {
+    // Require `accessibilityLabel` prop when `accessible` prop is `true`
+    // https://github.com/FormidableLabs/eslint-plugin-react-native-a11y/blob/master/docs/rules/accessibility-label.md
+    'react-native-a11y/accessibility-label': 'error',
+
+    // Require `<Touchable*>` components to have all a11y props
+    // https://github.com/FormidableLabs/eslint-plugin-react-native-a11y/blob/master/docs/rules/has-accessibility-props.md
+    'react-native-a11y/has-accessibility-props': 'error',
+
+    // Require valid a11y role for a11y-able elements
+    // https://github.com/FormidableLabs/eslint-pluginareact-native-a11y/blob/master/docs/rules/has-valid-accessibility-role.md
+    'react-native-a11y/has-valid-accessibility-role': 'error',
+
+    // Require accessibility states for a11y-able elements
+    // https://github.com/FormidableLabs/eslint-plugin-react-native-a11y/blob/master/docs/rules/has-valid-accessibility-states.md
+    'react-native-a11y/has-valid-accessibility-states': 'error',
+
+    // Require accessibility component type
+    // https://github.com/FormidableLabs/eslint-plugin-react-native-a11y/blob/master/docs/rules/has-valid-accessibility-component-type.md
+    'react-native-a11y/has-valid-accessibility-component-type': 'error',
+
+    // Require `accessibilityLiveRegion` prop for a11y-able dynamic elements
+    // https://github.com/FormidableLabs/eslint-plugin-react-native-a11y/blob/master/docs/rules/has-valid-accessibility-live-region.md
+    'react-native-a11y/has-valid-accessibility-live-region': 'error',
+
+    // Require overlapping views to have `importantForAccessibility` prop
+    // https://github.com/FormidableLabs/eslint-plugin-react-native-a11y/blob/master/docs/rules/has-valid-important-for-accessibility.md
+    'react-native-a11y/has-valid-important-for-accessibility': 'error',
+
+    // Prevent usage of `<Touchable*/>` without a11y elements
+    // https://github.com/FormidableLabs/eslint-plugin-react-native-a11y/blob/master/docs/rules/no-nested-touchables.md
+    'react-native-a11y/no-nested-touchables': 'error'
+  }
+}
--- packages/react-native/rules/react-native.js
+++ packages/react-native/rules/react-native.js
@@ -1,5 +1,44 @@
 /**
- * @todo
- *   Add React Native ESLint preset
+ * Rules for React Native
+ * https://github.com/Intellicode/eslint-plugin-react-native
  */
-module.exports = {}
+module.exports = {
+  plugins: [
+    '@xoi/eslint-config-react',
+    'react-native'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  env: {
+    // Allow all browser-like globals
+    'react-native/react-native': true
+  },
+  rules: {
+    // Detect unused `StyleSheet` rules
+    // https://github.com/Intellicode/eslint-plugin-react-native/blob/master/docs/rules/no-unused-styles.md
+    'react-native/no-unused-styles': 'error',
+
+    // Require style definitions to be sorted alphabetically
+    // https://github.com/Intellicode/eslint-plugin-react-native/blob/master/docs/rules/sort-styles.md
+    'react-native/sort-styles': 'off',
+
+    // Enforce using platform-specific filenames when necessary
+    // https://github.com/Intellicode/eslint-plugin-react-native/blob/master/docs/rules/split-platform-components.md
+    'react-native/split-platform-components': 'off',
+
+    // Disallow inline styles in JSX
+    // https://github.com/Intellicode/eslint-plugin-react-native/blob/master/docs/rules/no-inline-styles.md
+    'react-native/no-inline-styles': 'error',
+
+    // Disallow `StyleSheet` rules with color literals instead of variables
+    // https://github.com/Intellicode/eslint-plugin-react-native/blob/master/docs/rules/no-color-literals.md
+    'react-native/no-color-literals': 'error',
+
+    // Disallow text outside of `<Text>` component
+    // https://github.com/Intellicode/eslint-plugin-react-native/blob/master/docs/rules/no-raw-text.md
+    'react-native/no-raw-text': 'error'
+  }
+}
